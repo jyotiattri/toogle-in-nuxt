902964358170faf8673ed60894e7ac44f7cb14ee
jyoti attri <jyotiatri22@gmail.com>
2020-05-04 16:13:11 +0530

    create toggle in nuxt.js

---
+++ components/AppHeader.vue
@@ -0,0 +1,84 @@
+<template>
+  <header id="header">
+    <div class="logo">
+      <span class="icon fa-gem" />
+    </div>
+    <div class="content">
+      <div class="inner">
+        <h1>{{ title }}</h1>
+        <p>{{ data }}</p>
+      </div>
+    </div>
+    <div v-bind="toggleBodySection">
+      <nav>
+        <ul>
+          <li>
+            <nuxt-link to="#intro" value="option-1">
+              Intro
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="#work" value="option-2">
+              Work
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="#about" value="option-3">
+              About
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="#contact" value="option-4">
+              Contact
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      title: "Dimension",
+      data:
+        "A fully responsive site template designed by HTML5 UP and released for free under the Creative Commons license."
+    };
+  }
+};
+</script>
+<style>
+li {
+  display: inline-block;
+  padding: 10px;
+  border: 1px solid #000;
+  margin: 10px;
+}
+a {
+  color: #000;
+  text-decoration: none;
+}
+div#wrapper {
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px 0px;
+}
+article#intro {
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
+}
+article#work {
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
+}
+article#about {
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="wrapper">
+    <div>
+      <AppHeader />
+    </div>
+
+    <div id="main">
+      <!-- Intro -->
+      <article id="intro" value="option-1" name="body-section">
+        <h2 class="major">
+          Intro
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+          <a href="#work">awesome work</a>.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
+          rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per
+          conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh
+          porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc
+          ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit
+          sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris,
+          fringilla in aliquam at, euismod in lectus. Pellentesque habitant
+          morbi tristique senectus et netus et malesuada fames ac turpis
+          egestas. In non lorem sit amet elit placerat maximus. Pellentesque
+          aliquam maximus risus, vel sed vehicula.
+        </p>
+      </article>
+
+      <!-- Work -->
+      <article id="work" value="option-2" name="body-section">
+        <h2 class="major">
+          Work
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </article>
+      <!-- About -->
+      <article id="about" value="option-3" name="body-section">
+        <h2 class="major">
+          About
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
+          eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat
+          volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam.
+          Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero
+          et malesuada fames ac ante ipsum primis in faucibus. Cras viverra
+          ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.
+        </p>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader";
+export default {
+  name: 'Homepage',
+  components: {
+    AppHeader
+  },
+  methods: {
+  
+  toggleBodySection: {
+    get() {
+      return this.$route.query["toggle_body-section"] || "option-1";
+    },
+    set(newValue) {
+      this.$router.push({
+        path: "/",
+        query: {
+          ...this.queryStrings,
+          ...{ "toggle_body-section": newValue }
+        }
+      })
+    }
+  }
+}
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
